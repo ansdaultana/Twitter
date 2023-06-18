ae6901af3017ea45d6b50c40980f44163a5d789a
ansdaultana <116364572+ansdaultana@users.noreply.github.com>
2023-06-18 20:58:56 +0500

    Added Tweet As Comment. Models Migrations.

--- twitter/app/Http/Controllers/TweetController.php
+++ twitter/app/Http/Controllers/TweetController.php
@@ -16,32 +16,24 @@ use SebastianBergmann\Environment\Console;
 
 class TweetController extends Controller
 {
-    public function new()
-    {
-        return Inertia::render('Login');
-    }
+
 
     public function index()
     {
         $loggedInUser = auth()->user();
         $search = Request::input('search');
         $followingIds = $loggedInUser->following()->pluck('user_id');
-        //select tweets with user and likes so no n+1
         $Tweets = Tweet::with(['user', 'likes'])
-            //there is scope function in tweet model which returns the tweets of followingid
             ->fromFollowedUsers($followingIds)
-            ->withCount(['likes', 'comments'])
+            ->withCount(['likes', 'replies'])
             ->addSelect([
-                //counts the number of id values in the likes table and returns 1 if the count is greater than 0,
-                // indicating that there are likes present. Otherwise, it returns 0, indicating no likes.
                 'isLiked' => Like::selectRaw('IF(COUNT(id) > 0, 1, 0)')
-                    ->whereColumn('likeable_id', 'tweets.id')
-                    ->where('likeable_type', Tweet::class)
-                    ->where('user_id', $loggedInUser->id),
+                    ->whereColumn('tweet_id', 'tweets.id')
+                    ->where('user_id', $loggedInUser->id)
+                    ->limit(1),
             ])
             ->latest()
             ->get();
-
         return Inertia::render('Feed', [
             'users' => $search ? User::query()
                 ->where(function ($query) use ($search) {
@@ -110,31 +102,31 @@ class TweetController extends Controller
     {
         $loggedInUser = auth()->user();
         $search = Request::input('search');
-       
-        $Comments = Comment::with(['user', 'likes'])
-            ->withCount(['likes'])
-            ->addSelect([
-                'isLiked' => Like::selectRaw('IF(COUNT(id) > 0, 1, 0)')
-                    ->whereColumn('likeable_id', 'comments.id')
-                    ->where('likeable_type', Comment::class)
-                    ->where('user_id', $loggedInUser->id),
-            ])
-            ->latest()
-            ->get();
+
+        $replies = null;
+        //  Comment::with(['user', 'likes'])
+        //     ->withCount(['likes'])
+        //     ->addSelect([
+        //         'isLiked' => Like::selectRaw('IF(COUNT(id) > 0, 1, 0)')
+        //             ->where('likeable_type', Comment::class)
+        //             ->where('user_id', $loggedInUser->id),
+        //     ])
+        //     ->latest()
+        //     ->get();
         return Inertia::render(
             'ShowTweet',
             [
                 'tweet' => $tweet,
                 'users' => $search ? User::query()
-                ->where(function ($query) use ($search) {
-                    $query->where('name', 'like', '%' . $search . '%')
-                        ->orWhere('username', 'like', '%' . $search . '%');
-                })
-                ->where('id', '!=', auth()->user()->id)
-                ->limit(20)
-                ->get() : [],
-                'comments'=>$Comments,
-                'authUser'=>$loggedInUser
+                    ->where(function ($query) use ($search) {
+                        $query->where('name', 'like', '%' . $search . '%')
+                            ->orWhere('username', 'like', '%' . $search . '%');
+                    })
+                    ->where('id', '!=', auth()->user()->id)
+                    ->limit(20)
+                    ->get() : [],
+                'replies' => $replies,
+                'authUser' => $loggedInUser
             ]
         );
 
